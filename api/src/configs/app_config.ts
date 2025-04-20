df5c0d63d080d06fe5a0bf12168f1dae22ef9d27
Raul Battistini <raulbattistini.3@gmail.com>
2025-04-20 20:42:08 -0300

    chore: cleanup of various function calls

--- api/src/configs/app_config.ts
+++ api/src/configs/app_config.ts
@@ -1,55 +1,51 @@
-import { DataSourceOptions, DataSource } from "typeorm";
-import express from "express";
+import { DataSourceOptions } from "typeorm";
 import "dotenv/config";
-// import { ConnectionConfig } from "./db/connection_config";
 
 var appConfig: AppConfig = {
-    api: {
-        host: "",
-        port: 0,
-    },
-    connectionConfig: {
-        "type": "postgres"
-    }
+   api: {
+      host: "",
+      port: 0,
+   },
+   connectionConfig: {
+      type: "postgres",
+   },
 };
 
 export type ApiConfig = {
-    port: number;
-    host: string; 
-}
+   port: number;
+   host: string;
+};
 
 export type AppConfig = {
-    connectionConfig: DataSourceOptions,
-    api: ApiConfig,
+   connectionConfig: DataSourceOptions;
+   api: ApiConfig;
+};
+
+function loadConfig() {
+   const apiConfig: ApiConfig = {
+      port: 3333,
+      host: "localhost",
+   };
+   appConfig.api = apiConfig;
+
+   const connectionConfig: DataSourceOptions = {
+      type: "postgres",
+      database: "product",
+      host: "localhost",
+      port: 5432,
+      username: "root",
+      password: "postgres",
+   };
+
+   appConfig.connectionConfig = connectionConfig;
 }
-
-function loadConfig(){
-    const apiConfig: ApiConfig = {
-        port: 3333,
-        host: "localhost" 
-    }
-    appConfig.api = apiConfig;
-
-    const connectionConfig: DataSourceOptions = {
-        type: "postgres",
-        database: "product",
-        host: "localhost",
-        port: 5432,
-        username: "root",
-        password: "postgres"
-    }
-
-    appConfig.connectionConfig = connectionConfig;
-}
-
-loadConfig();
 
 function getApiConfig(): ApiConfig {
-    return appConfig.api;
+   return appConfig.api;
 }
 
 function getDbConfig(): DataSourceOptions {
-    return appConfig.connectionConfig;
+   return appConfig.connectionConfig;
 }
 
-export { getApiConfig, getDbConfig, loadConfig,  }
+export { getApiConfig, getDbConfig, loadConfig };
