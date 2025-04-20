bbbd3ce4751de3ba26e900322c6f0c123bffa0a7
Raul Battistini <raulbattistini.3@gmail.com>
2025-04-20 20:34:30 -0300

    feat: new errors treatment

---
+++ api/src/errors/client_errors/four_xx.error.ts
@@ -0,0 +1,29 @@
+import ZodErrorStatusEnum from "../../enums/errors_enums/errors_status.enum";
+// import { ClientSideErrorsEnum } from "../../enums/errors_enums/errors_status_codes.enum";
+import { BaseAppError } from "../global_error.error";
+
+const ValidationErrorsStatusCodeValues = ["400", "401", "403", "404", "422"] as const;
+type ValidationErrorsStatusCode = typeof ValidationErrorsStatusCodeValues[number];
+
+class ValidationError extends BaseAppError {
+    public statusCode: ValidationErrorsStatusCode;
+    public isOperational: false;
+    constructor(
+        statusCode: ValidationErrorsStatusCode,
+        message?: string
+    ) {
+        super(
+            statusCode,
+            ZodErrorStatusEnum.Values.error,
+            false,
+            message
+        );
+
+        this.statusCode = statusCode;
+        this.isOperational = false;
+
+        Object.setPrototypeOf(this, new.target.prototype);
+    }
+}
+
+export { ValidationError };
