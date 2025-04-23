ca202f9b7828dec07545c21534d0403249749598
Raul Battistini <raulbattistini.3@gmail.com>
2025-04-23 18:05:07 -0300

    chore: enhancing app configuration clarity

--- api/src/configs/app_config.ts
+++ api/src/configs/app_config.ts
@@ -1,15 +1,6 @@
-import { DataSourceOptions } from "typeorm";
+import { DataSource, DataSourceOptions } from "typeorm";
 import "dotenv/config";
-
-var appConfig: AppConfig = {
-   api: {
-      host: "",
-      port: 0,
-   },
-   connectionConfig: {
-      type: "postgres",
-   },
-};
+import { ServerError } from "../errors/server_errors/five_xx.error";
 
 export type ApiConfig = {
    port: number;
@@ -21,31 +12,58 @@ export type AppConfig = {
    api: ApiConfig;
 };
 
-function loadConfig() {
-   const apiConfig: ApiConfig = {
-      port: 3333,
-      host: "localhost",
-   };
-   appConfig.api = apiConfig;
-
-   const connectionConfig: DataSourceOptions = {
-      type: "postgres",
-      database: "product",
-      host: "localhost",
-      port: 5432,
-      username: "root",
-      password: "postgres",
-   };
-
-   appConfig.connectionConfig = connectionConfig;
+class Config {
+   private static instance: Config;
+   private appConfig: AppConfig;
+   private static connection: DataSource | null = null;
+   constructor() {
+      this.appConfig = {
+         api: this.loadApiConfig(),
+         connectionConfig: this.loadDbConfig(),
+      };
+   }
+   public static getInstance(): Config {
+      if (!Config.instance) {
+         Config.instance = new Config();
+      }
+      return Config.instance;
+   }
+   private loadApiConfig(): ApiConfig {
+      return {
+         port: Number(process.env.APP_PORT) || 3333,
+         host: String(process.env.APP_HOST) || "localhost",
+      };
+   }
+   private loadDbConfig(): DataSourceOptions {
+      const connectionOptions: DataSourceOptions = {
+         type: "postgres",
+         database: process.env.DB_NAME,
+         host: process.env.DB_HOST,
+         port: Number(process.env.DB_PORT) || 5432,
+         username: process.env.DB_USERNAME,
+         password: process.env.DB_PASSWORD,
+      };
+      return connectionOptions;
+   }
+   public getApiConfig(): ApiConfig {
+      return this.appConfig.api;
+   }
+   public getDbConfig(): DataSource {
+      try {
+         if (Config.connection && Config.connection.isInitialized) {
+            return Config.connection;
+         }
+         Config.connection = new DataSource(this.loadDbConfig());
+         Config.connection.initialize();
+         return Config.connection;
+      } catch (error) {
+         throw new ServerError(
+            "500",
+            true,
+            "Failure when connecting to the database",
+         );
+      }
+   }
 }
 
-function getApiConfig(): ApiConfig {
-   return appConfig.api;
-}
-
-function getDbConfig(): DataSourceOptions {
-   return appConfig.connectionConfig;
-}
-
-export { getApiConfig, getDbConfig, loadConfig };
+export default Config;
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,12 +1,23 @@
 import { app } from "./app";
-import { getApiConfig, loadConfig } from "./configs/app_config";
+import PathParams from "node:path";
+import Config from "./configs/app_config";
+import BaseErrorMiddleware from "./middlewares/global_middleware.middleware";
+import AppLogger from "./utils/logger/pino_logger.util";
 
 console.log("puta que pariu");
-loadConfig();
+const config = Config.getInstance();
 
-const host = getApiConfig().host;
-const port = getApiConfig().port;
+const host = config.getApiConfig().host;
+const port = config.getApiConfig().port;
+const connection = config.getDbConfig();
+const pinoLogger = new AppLogger();
+const baseErrorMiddleware = new BaseErrorMiddleware();
+app.use(baseErrorMiddleware.handle() as PathParams);
 
-app.listen(port, host, () => {
-    console.log(`Server listening on host: ${host} and port: ${port}`);
+app.listen(port, host, async () => {
+   if (!connection.isInitialized) {
+      await connection.initialize();
+   }
+   pinoLogger.handle({ applicationValues: config }, "DEBUG");
+   console.log(`Server listening on host: ${host} and port: ${port}`);
 });
